bc3667d99cbb33b54413c60844e85095266884d5
Дмитро Цибань <dmytro.tsyban@gmail.com>
2023-06-15 16:16:55 +0300

    Add project files.

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char sign = 'X';
+            Console.WriteLine(
+                "Welcome to the TicTacToe game!\n" +
+                "Now the field is empty:\n");
+            Console.WriteLine(
+                "\t" + "   |   |   \n" +
+                "\t" + "---+---+---\n" +
+                "\t" + "   |   |   \n" +
+                "\t" + "---+---+---\n" +
+                "\t" + "   |   |   \n");
+            Console.WriteLine("The game begins! Now you can play with X only");
+
+            void SetSign()
+            {
+                Console.Write("Choose the raw: ");
+                uint raw = Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Choose the column: ");
+                uint col = Convert.ToUInt32(Console.ReadLine());
+                Console.WriteLine();
+            }
+        }
+    }
+}
