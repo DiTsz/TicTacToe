6d3f01a1d0e0b3c6d04e5ece7861ee9740f6c023
Дмитро Цибань <dmytro.tsyban@gmail.com>
2023-06-16 23:40:49 +0300

    Created a field and a method to choose a cell

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,31 +1,63 @@
 ﻿using System;
+using System.Threading;
 
 namespace TicTacToe
 {
     internal class Program
     {
+        static char[,] field = new char[3, 3];
         static void Main(string[] args)
         {
-            char sign = 'X';
+            int row;
+            int col;
+
             Console.WriteLine(
                 "Welcome to the TicTacToe game!\n" +
                 "Now the field is empty:\n");
-            Console.WriteLine(
-                "\t" + "   |   |   \n" +
-                "\t" + "---+---+---\n" +
-                "\t" + "   |   |   \n" +
-                "\t" + "---+---+---\n" +
-                "\t" + "   |   |   \n");
-            Console.WriteLine("The game begins! Now you can play with X only");
 
-            void SetSign()
+            Console.Write("\t" + "   |   |   \n");
+            Console.Write("\t" + "---+---+---\n");
+            Console.Write("\t" + "   |   |   \n");
+            Console.Write("\t" + "---+---+---\n");
+            Console.Write("\t" + "   |   |  \n");
+            Console.WriteLine();
+
+            CellChoice();
+            Board();
+
+            void Board()
             {
-                Console.Write("Choose the raw: ");
-                uint raw = Convert.ToUInt32(Console.ReadLine());
-                Console.Write("Choose the column: ");
-                uint col = Convert.ToUInt32(Console.ReadLine());
-                Console.WriteLine();
+                Console.Write("\t" + " {0}  | {1} | {2}  \n", field[0, 0], field[0, 1], field[0, 2]);
+                Console.Write("\t" + "---+---+---\n");
+                Console.Write("\t" + " {0}  | {1}  | {2}  \n", field[1, 0], field[1, 1], field[1, 2]);
+                Console.Write("\t" + "---+---+---\n");
+                Console.WriteLine("\t" + " {0}  | {1}  | {2}  \n", field[2, 0], field[2, 1], field[2, 2]);
             }
+            Console.ReadLine();
+
+            // choose the cell
+
+            void CellChoice()
+            {
+                Console.WriteLine("Choice");
+                Console.Write("Row: ");
+                row = int.Parse(Console.ReadLine());
+                Console.Write("Column: ");
+                col = int.Parse(Console.ReadLine());
+                field[row-1, col-1] = 'X';
+            }
+
+            
+           
+
+            // 1 or 2 players
+
+
+            // peremoha algorythm
+           
+
+
+
         }
     }
 }
