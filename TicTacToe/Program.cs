d71d14da1b6db10595739da1a517bba14885cae2
Дмитро Цибань <dmytro.tsyban@gmail.com>
2023-06-17 18:36:18 +0300

    Rewrited from scratch. Completed version is ready to be played.

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,63 +1,118 @@
 ﻿using System;
-using System.Threading;
 
 namespace TicTacToe
 {
-    internal class Program
+    class Program
     {
-        static char[,] field = new char[3, 3];
+        // creat a board with rows and columns
+        static char[,] board = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+        // X is set by default, his move is the first move
+        static char currentPlayer = 'X';
+
         static void Main(string[] args)
         {
-            int row;
-            int col;
+            bool gameFinished = false;
 
-            Console.WriteLine(
-                "Welcome to the TicTacToe game!\n" +
-                "Now the field is empty:\n");
-
-            Console.Write("\t" + "   |   |   \n");
-            Console.Write("\t" + "---+---+---\n");
-            Console.Write("\t" + "   |   |   \n");
-            Console.Write("\t" + "---+---+---\n");
-            Console.Write("\t" + "   |   |  \n");
-            Console.WriteLine();
-
-            CellChoice();
-            Board();
-
-            void Board()
+            while (!gameFinished)
             {
-                Console.Write("\t" + " {0}  | {1} | {2}  \n", field[0, 0], field[0, 1], field[0, 2]);
-                Console.Write("\t" + "---+---+---\n");
-                Console.Write("\t" + " {0}  | {1}  | {2}  \n", field[1, 0], field[1, 1], field[1, 2]);
-                Console.Write("\t" + "---+---+---\n");
-                Console.WriteLine("\t" + " {0}  | {1}  | {2}  \n", field[2, 0], field[2, 1], field[2, 2]);
-            }
-            Console.ReadLine();
+                // shows empty board
+                Board();
 
-            // choose the cell
-
-            void CellChoice()
-            {
-                Console.WriteLine("Choice");
+                // choosing a cell to put X or O in
+                Console.WriteLine("Player {0}, choose the cell.", currentPlayer);
                 Console.Write("Row: ");
-                row = int.Parse(Console.ReadLine());
+                int row = int.Parse(Console.ReadLine());
                 Console.Write("Column: ");
-                col = int.Parse(Console.ReadLine());
-                field[row-1, col-1] = 'X';
+                int col = int.Parse(Console.ReadLine());
+
+                if (IsValidMove(row, col))
+                {
+                    board[row - 1, col - 1] = currentPlayer;
+                    if (IsWinningMove())
+                    {
+                        Board();
+                        Console.WriteLine("Player {0} wins!", currentPlayer);
+                        gameFinished = true;
+                    }
+                    else if (IsBoardFull())
+                    {
+                        Board();
+                        Console.WriteLine("It's a draw!");
+                        gameFinished = true;
+                    }
+                    else
+                    {
+                        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid move. Please try again.");
+                }
             }
 
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static void Board()
+        {
+            Console.Clear();
+            Console.WriteLine(" {0} | {1} | {2} ", board[0,0], board[0,1], board[0,2]);
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" {0} | {1} | {2} ", board[1,0], board[1,1], board[1,2]);
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" {0} | {1} | {2} ", board[2,0], board[2,1], board[2,2]);
+        }
+
+        // check, whether the move is appropriate
+        static bool IsValidMove(int r, int c)
+        {
+            if ((r < 1 || r > 3) && (c < 1 || c > 3) || (board[r - 1, c - 1] != ' '))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        static bool IsWinningMove()
+        {
+            // Check rows
             
-           
+            for (int j = 0; j < 3; j++)
+            {
+                if (board[0,j] == currentPlayer && board[1,j] == currentPlayer && board[2,j] == currentPlayer)
+                    return true;
+            }
+            
 
-            // 1 or 2 players
+            // Check columns
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i,0] == currentPlayer && board[i,1] == currentPlayer && board[i,2] == currentPlayer)
+                    return true;
+            }
 
+            // Check diagonals
+            if ((board[0,0] == currentPlayer && board[1,1] == currentPlayer && board[2,2] == currentPlayer) ||
+                (board[0,2] == currentPlayer && board[1,1] == currentPlayer && board[2,0] == currentPlayer))
+                return true;
 
-            // peremoha algorythm
-           
+            return false;
+        }
 
-
-
+        // if the board is full, but there is no winner, the game must stop in a draw 
+        static bool IsBoardFull()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == ' ')
+                        return false;
+                }
+            }
+            return true;
         }
     }
 }
